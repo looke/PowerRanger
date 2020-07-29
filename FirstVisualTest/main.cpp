103a7c843ad6ed3a0d4ba7a37ba4cf33333d6178
looke <52668057@qq.com>
2020-07-29 16:50:35 +0800

    update

--- FirstVisualTest/main.cpp
+++ FirstVisualTest/main.cpp
@@ -10,6 +10,23 @@ using namespace sl;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    //test cuda code
+    int num_devices = cv::cuda::getCudaEnabledDeviceCount();
+    cout << "Cuda Enabled Device Count:" << num_devices << endl;
+
+    //test ZED camera
+    sl::Camera zed;
+    sl::ERROR_CODE err = zed.open();
+    if(err != ERROR_CODE::SUCCESS)
+    {
+        std::cout << err << endl;
+    }
+
+    cv::Mat img = cv::imread("/home/nvidia/QT_Projects/OpenCV_Test3/cat.jpg");
+    cv::line(img, cv::Point(1,1), cv::Point(100,100), cv::Scalar(0,0,255),5); //draw a line
+    cv::imshow("Hello Opencv World!", img);
+    cv::waitKey(0);
+
+    cout << "Hello Visual World!" << endl;
     return 0;
 }
