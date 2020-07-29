54eccdf6f885e02fa8d3b68a91608f61db6d7a89
looke <52668057@qq.com>
2020-07-29 14:36:16 +0800

    add visual test code include files

--- FirstVisualTest/main.cpp
+++ FirstVisualTest/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
+#include "opencv2/opencv.hpp"
+#include "opencv2/core/cuda.hpp"
+#include "sl/Camera.hpp"
 
 using namespace std;
+using namespace cv;
+using namespace cv::cuda;
+using namespace sl;
 
 int main()
 {
